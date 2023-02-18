f85068c75ea1c0f6f6479c88a4b05bb82464bb04
unknown <levi.millikin@gmail.com>
2023-02-18 03:59:24 -0800

    first commit

---
+++ hardhat-nftcollection/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x979FC131047F887A958D00359beA15287311129b";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://whitelist-d-app-next-ui.vercel.app//api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
