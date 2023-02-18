d6892b5389e7584454f38437168ef53e7e223deb
unknown <levi.millikin@gmail.com>
2023-02-18 04:16:55 -0800

    my name

--- nftcollectionui/src/pages/index.js
+++ nftcollectionui/src/pages/index.js
@@ -354,7 +354,7 @@ export default function Home() {
       </Head>
       <div className={styles.main}>
         <div>
-          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+          <h1 className={styles.title}>Welcome to levimilli&apos;s nft collection!</h1>
           <div className={styles.description}>
             Its an NFT collection for developers in Crypto.
           </div>
@@ -369,7 +369,7 @@ export default function Home() {
       </div>
 
       <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
+        Made by levimilli 🔥
       </footer>
     </div>
   );
